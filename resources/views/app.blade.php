3bed227f4000cf31a29a88046e83c523160892f6
James Thompson <james.thompson@oise.com>
2015-11-19 11:43:04 +0000

    Initial design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,12 +28,14 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="#">Media App</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="navbar">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Welcome</a></li>
+					<li><a href="{{ url('/') }}">Home</a></li>
+                                        <li><a href="{{ url('media') }}">View Media</a></li>
+                                        <li><a href="{{ url('media/create') }}">Create Media</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
@@ -56,9 +58,11 @@
 			</div>
 		</div>
 	</nav>
-
+        
+        <div class="container">
 	@yield('content')
-
+        </div>
+        
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
